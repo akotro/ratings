363d7f427220b32342f1cc04db07dc7246c78635
Antonis Kotronakis <mail@akotro.dev>
2025-10-31 17:53:42 +0000

    fix(flake): replace node2nix with buildNpmPackage for frontend
    Also removes automatic deploy actions

--- flake.nix
+++ flake.nix
@@ -13,89 +13,89 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    gitignore,
-    crane,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      gitignore,
+      crane,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      inherit (pkgs) lib;
+        inherit (pkgs) lib;
 
-      craneLib = crane.lib.${system};
+        craneLib = crane.lib.${system};
 
-      sqlFilter = path: _type: null != builtins.match ".*sql$" path;
-      sqlOrCargo = path: type: (sqlFilter path type) || (craneLib.filterCargoSources path type);
-      src = lib.cleanSourceWith {
-        src = craneLib.path ./.;
-        filter = sqlOrCargo;
-      };
+        sqlFilter = path: _type: null != builtins.match ".*sql$" path;
+        sqlOrCargo = path: type: (sqlFilter path type) || (craneLib.filterCargoSources path type);
+        src = lib.cleanSourceWith {
+          src = craneLib.path ./.;
+          filter = sqlOrCargo;
+        };
 
-      nativeBuildInputs = with pkgs; [pkg-config];
-      buildInputs = with pkgs; [perl openssl mariadb];
+        nativeBuildInputs = with pkgs; [ pkg-config ];
+        buildInputs = with pkgs; [
+          perl
+          openssl
+          mariadb
+        ];
 
-      commonArgs = {
-        inherit src nativeBuildInputs buildInputs;
-        pname = "ratings";
-        version = "0.1.0";
-        doCheck = false;
-      };
+        commonArgs = {
+          inherit src nativeBuildInputs buildInputs;
+          pname = "ratings";
+          version = "0.1.0";
+          doCheck = false;
+        };
 
-      # Build *just* the cargo dependencies, so we can reuse
-      # all of that work (e.g. via cachix) when running in CI
-      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-      # Build the actual crate itself, reusing the dependency
-      # artifacts from above.
-      ratings = craneLib.buildPackage (commonArgs
-        // {
-          inherit cargoArtifacts;
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        ratings = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
 
-          nativeBuildInputs =
-            (commonArgs.nativeBuildInputs or [])
-            ++ [
+            nativeBuildInputs = (commonArgs.nativeBuildInputs or [ ]) ++ [
               pkgs.sqlx-cli
             ];
 
-          preBuild = ''
-            export SQLX_OFFLINE_DIR=${craneLib.path ./.sqlx}
+            preBuild = ''
+              export SQLX_OFFLINE_DIR=${craneLib.path ./.sqlx}
+            '';
+          }
+        );
+
+        frontend = pkgs.buildNpmPackage {
+          name = "frontend";
+          src = gitignore.lib.gitignoreSource ./frontend;
+
+          npmDepsHash = "sha256-Rj/eQilByhhhTZvkxpzXx77uKZPuVnArMavL2b6pAVs=";
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out
+            cp -r build/* $out/
+
+            runHook postInstall
           '';
-        });
+        };
 
-      frontendNodeDependencies = (pkgs.callPackage ./frontend/default.nix {}).nodeDependencies;
-      frontend = pkgs.stdenv.mkDerivation {
-        name = "frontend";
-        src = gitignore.lib.gitignoreSource ./frontend;
-        buildInputs = [pkgs.nodejs];
-        buildPhase = ''
-          ln -s ${frontendNodeDependencies}/lib/node_modules ./node_modules
-          export PATH="${frontendNodeDependencies}/bin:$PATH"
-
-          npm run build
-
-          mkdir -p $out/build
-          cp -r build $out/
-
-          cp -r ./package*.json $out/
-          cp -r ./node_modules $out/
-        '';
-      };
-    in {
-      checks = {
-        # Build the crate as part of `nix flake check` for convenience
-        inherit ratings;
-      };
-      packages = {
-        inherit ratings frontend;
-
-        default = pkgs.stdenv.mkDerivation {
+        full = pkgs.stdenv.mkDerivation {
           name = "ratings";
           src = gitignore.lib.gitignoreSource ./.;
-          buildInputs = [ratings frontend];
+          buildInputs = [
+            ratings
+            frontend
+          ];
 
           buildPhase = "true";
 
@@ -105,21 +105,27 @@
             cp -r ${frontend}/* $out/frontend/
           '';
         };
-      };
-      # devShells.default = craneLib.devShell {
-      # checks = self.checks.${system};
-      # Additional dev-shell environment variables can be set directly
-      # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+      in
+      {
+        checks = {
+          inherit ratings frontend;
+          default = full;
+        };
 
-      # RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        packages = {
+          inherit ratings frontend;
 
-      devShells.default = pkgs.mkShell {
-        packages = [
-          pkgs.nil
-          pkgs.node2nix
-          pkgs.sqlx-cli
-          pkgs.libmysqlclient.dev
-        ];
-      };
-    });
+          default = full;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nil
+            pkgs.node2nix
+            pkgs.sqlx-cli
+            pkgs.libmysqlclient.dev
+          ];
+        };
+      }
+    );
 }
