72a8d4fb7c30f21792e56fcec7a5caf514d7f15d
Antonis Kotronakis <mail@akotro.dev>
2025-11-12 17:46:27 +0000

    feat(nix): use devenv to setup mysql db

--- flake.nix
+++ flake.nix
@@ -1,131 +1,222 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    crane = {
-      url = "github:ipetkov/crane";
+    crane.url = "github:ipetkov/crane";
+    devenv = {
+      url = "github:cachix/devenv";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
   outputs =
-    {
-      self,
-      nixpkgs,
-      gitignore,
-      crane,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        inherit (pkgs) lib;
-
-        craneLib = crane.lib.${system};
-
-        sqlFilter = path: _type: null != builtins.match ".*sql$" path;
-        sqlOrCargo = path: type: (sqlFilter path type) || (craneLib.filterCargoSources path type);
-        src = lib.cleanSourceWith {
-          src = craneLib.path ./.;
-          filter = sqlOrCargo;
-        };
-
-        nativeBuildInputs = with pkgs; [ pkg-config ];
-        buildInputs = with pkgs; [
-          perl
-          openssl
-          mariadb
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { self, ... }:
+      {
+        imports = [
+          inputs.devenv.flakeModule
         ];
 
-        commonArgs = {
-          inherit src nativeBuildInputs buildInputs;
-          pname = "ratings";
-          version = "0.1.0";
-          doCheck = false;
-        };
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ];
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        perSystem =
+          {
+            config,
+            self',
+            inputs',
+            pkgs,
+            system,
+            ...
+          }:
+          let
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ (import inputs.rust-overlay) ];
+            };
+            inherit (pkgs) lib;
+            rust = pkgs.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            };
+            craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (_: rust);
 
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
-        ratings = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
+            unfilteredRoot = ./.;
+            src = lib.fileset.toSource {
+              root = unfilteredRoot;
+              fileset = lib.fileset.unions [
+                (craneLib.fileset.commonCargoSources unfilteredRoot)
+                ./migrations
+                ./.sqlx
+              ];
+            };
 
-            nativeBuildInputs = (commonArgs.nativeBuildInputs or [ ]) ++ [
-              pkgs.sqlx-cli
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+            buildInputs = with pkgs; [
+              perl
+              openssl
+              mariadb
             ];
 
-            preBuild = ''
-              export SQLX_OFFLINE_DIR=${craneLib.path ./.sqlx}
-            '';
-          }
-        );
+            commonArgs = {
+              inherit src nativeBuildInputs buildInputs;
+              pname = "ratings";
+              version = "0.1.0";
+              doCheck = false;
+            };
 
-        frontend = pkgs.buildNpmPackage {
-          name = "frontend";
-          src = gitignore.lib.gitignoreSource ./frontend;
+            # Build *just* the cargo dependencies, so we can reuse
+            # all of that work (e.g. via cachix) when running in CI
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-          npmDepsHash = "sha256-Rj/eQilByhhhTZvkxpzXx77uKZPuVnArMavL2b6pAVs=";
+            # Build the actual crate itself, reusing the dependency
+            # artifacts from above.
+            ratings = craneLib.buildPackage (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
 
-          installPhase = ''
-            runHook preInstall
+                nativeBuildInputs = (commonArgs.nativeBuildInputs or [ ]) ++ [
+                  pkgs.sqlx-cli
+                ];
 
-            mkdir -p $out
-            cp -r build/* $out/
+                preBuild = ''
+                  export SQLX_OFFLINE=true
+                  export SQLX_OFFLINE_DIR=${craneLib.path ./.sqlx}
+                '';
+              }
+            );
 
-            runHook postInstall
-          '';
-        };
+            frontend = pkgs.buildNpmPackage {
+              name = "frontend";
+              src = ./frontend;
 
-        full = pkgs.stdenv.mkDerivation {
-          name = "ratings";
-          src = gitignore.lib.gitignoreSource ./.;
-          buildInputs = [
-            ratings
-            frontend
-          ];
+              npmDepsHash = "sha256-DGFWzjHVvYmEF4zeB0XQaNyUfJBSsnl/0GF0rwNvGcI=";
 
-          buildPhase = "true";
+              installPhase = ''
+                runHook preInstall
 
-          installPhase = ''
-            mkdir -p $out/backend $out/frontend
-            cp -r ${ratings}/* $out/backend/
-            cp -r ${frontend}/* $out/frontend/
-          '';
-        };
-      in
-      {
-        checks = {
-          inherit ratings frontend;
-          default = full;
-        };
+                mkdir -p $out
+                cp -r build/* $out/
 
-        packages = {
-          inherit ratings frontend;
+                runHook postInstall
+              '';
+            };
 
-          default = full;
-        };
+            full = pkgs.stdenv.mkDerivation {
+              name = "ratings";
+              src = ./.;
+              buildInputs = [
+                ratings
+                frontend
+              ];
 
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.nil
-            pkgs.node2nix
-            pkgs.sqlx-cli
-            pkgs.libmysqlclient.dev
-          ];
-        };
+              buildPhase = "true";
+
+              installPhase = ''
+                mkdir -p $out/backend $out/frontend
+                cp -r ${ratings}/* $out/backend/
+                cp -r ${frontend}/* $out/frontend/
+              '';
+            };
+          in
+          {
+            formatter = pkgs.nixfmt-tree;
+
+            checks = {
+              inherit ratings frontend;
+              default = full;
+            };
+
+            packages = {
+              inherit ratings frontend;
+
+              default = full;
+            };
+
+            devenv.shells.default = {
+              # https://devenv.sh/reference/options/
+              packages = [
+                pkgs.pkg-config
+                pkgs.openssl
+                pkgs.gdb
+                pkgs.lldb
+                pkgs.vscode-extensions.vadimcn.vscode-lldb
+
+                rust
+                pkgs.rust-bin.stable.latest.rust-analyzer
+                pkgs.sqlx-cli
+                pkgs.libmysqlclient.dev
+
+                pkgs.nodejs
+                pkgs.svelte-language-server
+                pkgs.tailwindcss-language-server
+                pkgs.typescript-language-server
+                pkgs.vtsls
+              ];
+
+              env = {
+                DATABASE_URL = "mysql://local:@127.0.0.1:3306/ratings";
+              };
+
+              services = {
+                mysql = {
+                  enable = true;
+                  package = pkgs.mariadb;
+                  settings.mysqld = {
+                    bind-address = "127.0.0.1";
+                  };
+                  initialDatabases = [
+                    { name = "ratings"; }
+                  ];
+                  ensureUsers = [
+                    {
+                      name = "local";
+                      ensurePermissions = {
+                        "*.*" = "ALL PRIVILEGES";
+                      };
+                    }
+                  ];
+                };
+              };
+
+              processes = {
+                mysql-configure.process-compose = {
+                  ready_log_line = "Adding user: local";
+                };
+                migrate = {
+                  exec = # bash
+                    ''
+                      echo "Running migrations..."
+                      ${lib.getExe pkgs.sqlx-cli} migrate run
+                    '';
+                  process-compose = {
+                    depends_on = {
+                      mysql-configure = {
+                        condition = "process_log_ready";
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
       }
     );
 }
